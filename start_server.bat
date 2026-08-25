0d72cb6ba10021912ffa65cfe5cd4428ca62fa6c
Hammad55553 <hammadaslam78612@gmail.com>
2026-08-25 12:04:19 +0500

    Add batch script to start backend server

---
+++ start_server.bat
@@ -0,0 +1,9 @@
+@echo off
+title TEHZEEB POS - Backend Server
+echo ===================================================
+echo Starting TEHZEEB POS Backend Server...
+echo Please DO NOT close this window while the app is running!
+echo ===================================================
+cd backend
+python run.py
+pause
