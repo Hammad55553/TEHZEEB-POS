95c6112956bff08afbc477774fac1dd0e759c460
Hammad <hammadaslam78612@gmail.com>
2026-08-31 13:37:20 +0000

    Remove stray scratch scripts (fix_db2, test_insert) + ignore them

--- fix_db2.py
+++
@@ -1,7 +0,0 @@
-from backend.app.db import init_pool, get_cursor
-
-init_pool()
-
-with get_cursor(commit=True) as cur:
-    cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'shortage';")
-    print([r['column_name'] for r in cur.fetchall()])
